b84dcbd26272867c8a98a5470d9f61860e3e5a11
Said <saidemad098@gmail.com>
2023-12-18 20:40:38 +0200

    homeLayout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:elmoazen/homeLayout.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-
+      home: homeLayout(),
     );
   }
 }
